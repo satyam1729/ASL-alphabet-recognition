1d1bbdc5472ef5fa30b9421a43dddab27f741bc1
Satyam Yadav <satyamyadav13579@gmail.com>
2018-04-13 00:24:55 +0530

    utils.py to keep data into folders && executed utils.py

--- utils.py
+++ utils.py
@@ -7,3 +7,18 @@ Created on Thu Apr 12 19:50:42 2018
 """
 
 import os
+import pathlib
+pwd=os.getcwd()+'/dataset/'
+directories=os.listdir(pwd)
+from string import ascii_lowercase
+for c in ascii_lowercase:
+    pathlib.Path(pwd+c).mkdir(parents=True, exist_ok=True)
+for i in range(10):
+    pathlib.Path(pwd+str(i)).mkdir(parents=True, exist_ok=True)
+    
+
+for directory in directories:
+    files=os.listdir(pwd+directory)
+    for file in files:
+        os.rename(pwd+directory+'/'+file,pwd+file[6]+'/'+file)
+    os.rmdir(pwd+directory)
